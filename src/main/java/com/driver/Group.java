a0d42ba842f55740a62ae3a01e0860cdd26395c8
Yash Pawar <yashpawar.yp5@gmail.com>
2024-05-04 23:58:25 +0530

    submitted assignment

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,24 @@
 package com.driver;
 
 public class Group {
-    private String name;
-    private int numberOfParticipants;
 
+    private String name;
+
+    public Group(String groupName, int numberOfParticipants) {
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private int numberOfParticipants;
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -3,8 +3,35 @@ package com.driver;
 import java.util.Date;
 
 public class Message {
+
     private int id;
+
+    public Message(int messageId, String content) {
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
     private String content;
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     private Date timestamp;
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
 
 }
